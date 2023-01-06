54f821f8bc48199dd456b0aeea26fa9d18604ffd
groupp9 <122083110+groupp9@users.noreply.github.com>
2023-01-06 14:58:51 +0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const inputUsername = document.querySelector(".input-login-username");
+const inputPassword = document.querySelector(".input-login-password");
+const btnLogin = document.querySelector(".login__signInButton");
+btnLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (inputUsername.value === "" || inputPassword.value === "") {
+    alert("Vui lòng không để trống");
+  } else {
+    const user = JSON.parse(localStorage.getItem(inputUsername.value));
+    if (
+      user.username === inputUsername.value &&
+      user.password === inputPassword.value
+    ) {
+      alert("Đăng Nhập Thành Công");
+      window.location.href = "https://pndangkhoa274.github.io/MainPage/";
+    } else {
+      alert("Đăng Nhập Thất Bại");
+    }
+  }
+});
